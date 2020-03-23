4de4a60d21f4aef5352dca2dbf051d4d77eb3415
Jonathan Angeles <21043431+jangelesg@users.noreply.github.com>
2020-03-23 11:05:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='cov1d19-cli',
     version='1.0.0',
     packages=[''],
-    url='',
+    url='https://github.com/jangelesg/cov1d19-cli',
     license='GPL ',
     author='Jonathan Angeles',
     author_email='jangelesg@gangsecurity.com',
-    description='COVID19 TRACK TOOL'
+    description='COVID19 TRACK TOOL', install_requires=['prettytable', 'colorama', 'pyfiglet', 'requests', 'urllib3']
 )
+
